cef701f4da324b755f7f332202aad43b911f0d8a
kujbusadamattila120 <adamka20080920@gmail.com>
2026-05-08 13:40:24 +0200

    keveres es ujrataoltes + range beallitasa

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-var jelek = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D'];
+var jelek = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
 var palya = document.getElementById('palya');
+var csuszka = document.getElementById('csuszka');
+var ertekSzoveg = document.getElementById('ertek');
+var gomb = document.getElementById('keveres');
 
-for (var i = 0; i < 8; i++) {
-    var div = document.createElement('div');
-    div.className = 'kartya';
-    div.innerHTML = '?';
-    div.id = i;
+function jatekInditasa() {
+    palya.innerHTML = '';
+    var darab = csuszka.value;
+    
+    var aktualisJelek = [];
+    for (var i = 0; i < darab; i++) {
+        aktualisJelek.push(jelek[i]);
+    }
 
-    div.onclick = function() {
-        var sorszam = this.id;
-        this.innerHTML = jelek[sorszam];
-        this.style.backgroundColor = 'white';
-    };
+    aktualisJelek.sort(function() { return 0.5 - Math.random(); });
 
-    palya.appendChild(div);
+    for (var i = 0; i < darab; i++) {
+        var div = document.createElement('div');
+        div.className = 'kartya';
+        div.innerHTML = '?';
+        div.id = i;
+        
+        div.setAttribute('data-jel', aktualisJelek[i]);
+
+        div.onclick = function() {
+            this.innerHTML = this.getAttribute('data-jel');
+            this.style.backgroundColor = 'white';
+        };
+
+        palya.appendChild(div);
+    }
 }
+
+
+csuszka.oninput = function() {
+    ertekSzoveg.innerHTML = this.value;
+}
+
+
+gomb.onclick = function() {
+    jatekInditasa();
+}
+
+jatekInditasa();
